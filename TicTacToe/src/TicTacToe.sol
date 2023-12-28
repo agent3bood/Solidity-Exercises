4178802d8df0457278f6c5f67e8ed8c7d46a0c8a
Abdullah Alsigar <abdullah.alsigar@gmail.com>
2023-12-28 19:59:40 +0300

    nested arrays

--- TicTacToe/src/TicTacToe.sol
+++ TicTacToe/src/TicTacToe.sol
@@ -11,6 +11,15 @@ contract TicTacToe {
     */
 
     function isWinning(uint8[3][3] memory board) public pure returns (bool) {
-        // your code here
+        return ((board[0][0] == board[0][1]) && (board[0][1] == board[0][2])) ||
+        ((board[1][0] == board[1][1]) && (board[1][1] == board[1][2])) ||
+        ((board[2][0] == board[2][1]) && (board[2][1] == board[2][2])) ||
+        //
+       ((board[0][0] == board[1][0]) && (board[1][0] == board[2][0])) ||
+        ((board[0][1] == board[1][1]) && (board[1][1] == board[2][1])) ||
+        ((board[0][2] == board[1][2]) && (board[1][2] == board[2][2])) ||
+        //
+        ((board[0][0] == board[1][1]) && (board[1][1] == board[2][2])) ||
+            ((board[0][2] == board[1][1]) && (board[1][1] == board[2][0]));
     }
 }
