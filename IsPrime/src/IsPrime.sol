90b37bb9cb30b7db4303d38e481062c365788c87
Abdullah Alsigar <abdullah.alsigar@gmail.com>
2023-12-28 18:16:31 +0300

    prime

--- IsPrime/src/IsPrime.sol
+++ IsPrime/src/IsPrime.sol
@@ -6,6 +6,14 @@ contract IsPrime {
      * The goal of this exercise is to return if "number" is prime or not (true or false)
      */
     function isPrime(uint256 number) public view returns (bool) {
-        // your code here
+        if (number <= 2) return true;
+        if (number % 2 == 0) return false;
+
+        for (uint i = 3; i * i <= number; i += 2) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
